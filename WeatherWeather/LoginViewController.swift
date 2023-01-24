a69925ba7129b2837ae1e879e1d10e5df6b267e8
lopatinmaks <lopatin.maks2611@gmail.com>
2023-01-24 16:30:26 +0400

    LessonTwo

--- WeatherWeather/LoginViewController.swift
+++ WeatherWeather/LoginViewController.swift
@@ -13,9 +13,20 @@ final class LoginViewController: UIViewController {
     @IBOutlet private var scrollView: UIScrollView!
     @IBOutlet private var loginTextField: UITextField!
     @IBOutlet private var passwordTextField: UITextField!
-        
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Login VC loaded")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Login VC did appear")
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        print("Login will appear")
             
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShown(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
             
@@ -25,7 +36,7 @@ final class LoginViewController: UIViewController {
         scrollView.addGestureRecognizer(tapGesture)
     }
         
-    @objc func keyboardWillShown(notification: Notification) {
+    @objc private func keyboardWillShown(notification: Notification) {
         let info = notification.userInfo as? NSDictionary
         let kbSize = (info?.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as? NSValue)?.cgRectValue.size
             
@@ -35,31 +46,66 @@ final class LoginViewController: UIViewController {
         scrollView.scrollIndicatorInsets = contentInsets
     }
         
-    @objc func keyboardWillHide(notification: Notification) {
+    @objc private func keyboardWillHide(notification: Notification) {
         scrollView.contentInset = UIEdgeInsets.zero
         scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#function)
     }
         
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        print("Login VC will disappear")
             
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
     }
         
-    @objc func hideKeyboard() {
+    @objc private func hideKeyboard() {
         scrollView.endEditing(true)
     }
-
-    @IBAction func login(_ sender: UIButton) {
+    
+    private func checkLoginInfo() -> Bool {
         guard let loginText = loginTextField.text,
-              let passwordText = passwordTextField.text else { return }
-
+              let passwordText = passwordTextField.text else { return false }
+        
         if loginText == "admin", passwordText == "12345" {
             print("Успешно!")
+            return true
         } else {
             print("Попробуй еще раз!")
+            return false
         }
     }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        if identifier == "loginSegue" {
+            if checkLoginInfo() {
+                return true
+            } else {
+                showLoginError()
+                return false
+            }
+        }
+        return true
+    }
+    
+    private func showLoginError() {
+        let alert = UIAlertController(title: "Ошибка", message: "Логин и/или пароль не верны!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    @IBAction func liginTapped(_ sender: UIButton) {
+        guard let helloVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "helloVC") as? HelloViewController else { return }
+        helloVC.modalPresentationStyle = .fullScreen
+        
+        present(helloVC, animated: true)
+    }
 }
-
