d2fafb279483953ee74126036bac9e6c5c3049fd
lopatinmaks <lopatin.maks2611@gmail.com>
2023-03-27 13:21:54 +0400

    Урок1

---
+++ WeatherWeather/Session.swift
@@ -0,0 +1,21 @@
+//
+//  Session.swift
+//  WeatherWeather
+//
+//  Created by Ольга on 27.03.2023.
+//
+
+import Foundation
+
+class Session: CustomStringConvertible {
+    static let shared = Session()
+    private init() {}
+    
+    var id: Int = 0
+    var userName: String = ""
+    var balance: Int = 0
+    
+    var description: String {
+        return "id: \(id), userName: \(userName), balance: \(balance)$"
+    }
+}
